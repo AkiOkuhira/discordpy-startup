1e536081ad3cf5aa96cff190a95f53957cf1c084
AkiOkuhira <65722564+AkiOkuhira@users.noreply.github.com>
2020-05-22 01:55:32 +0700

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,20 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='')
 token = os.environ['DISCORD_BOT_TOKEN']
+
+ohayo = ['おはようございます', 'ふふ、もう昼ですよ', 'おはようございます、お寝坊さんですね', 'おはようございます。朝食はもう取りましたか？', 'おはようございます、今日も一緒に頑張りましょうね']
+errorsMessages = ['すみません、理解できません', 'おやおや、何かお忘れでは？']
 
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    await ctx.send(random.choice(errorsMessages) \n error_msg)
 
 
 @bot.command()
@@ -18,7 +22,7 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
-ohayo = ['おはようございます', 'ふふ、もう昼ですよ', 'おはようございます、お寝坊さんですね', 'おはようございます。朝食はもう取りましたか？', 'おはようございます、今日も一緒に頑張りましょうね']
+
 @bot.command()
 async def おはよう(ctx):
     await ctx.send(random.choice(ohayo))
